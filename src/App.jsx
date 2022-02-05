f09b78e98c53dbf2b22943e20a34e45daf28b55e
Armando Garcia <armgarcia@sofi.org>
2022-02-04 23:33:08 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    fetch("http://localhost:4000", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `
+      query {
+        allPersons{
+          name
+          phone
+        }
+      }
+        `,
+      }),
+    })
+      .then(res => res.json())
+      .then(res => console.table(res.data));
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>GraphQL + React!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
