7095534c5b498bd485090c67310bdcb2bb28fa08
Armando Garcia <armgarcia@sofi.org>
2022-02-06 23:45:08 -0600

    useQuery, useLazyQuery from apollo client

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+import { Persons } from "./Persons";
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      id
+      name
+      phone
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
 
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:4000", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `
-      query {
-        allPersons{
-          name
-          phone
-        }
-      }
-        `,
-      }),
-    })
-      .then(res => res.json())
-      .then(res => console.log(res.data));
-  });
+  const { data, error, loading } = useQuery(ALL_PERSONS);
+
+  // console.log(data);
+
+  if (error) return <span style="color: red">{error}</span>;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>GraphQL + React!</p>
+
+        {loading ? <p>Loading...</p> : <Persons persons={data?.allPersons} />}
       </header>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloClient,
+  HttpLink,
+  gql,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+  }),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </ApolloProvider>,
+  document.getElementById("root")
+);
